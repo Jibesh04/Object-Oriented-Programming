a934032d16399cb70ac954e1c91ad31d305572de
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2022-11-03 01:35:35 +0530

    Unary Operator Overloading using friend function

---
+++ operator_overloading2.cpp
@@ -0,0 +1,37 @@
+/* Do the same program using friend function */
+/* Write a program to overload unary operator - (minus) using member function. */
+#include <iostream>
+using namespace std;
+class number{
+        int a, b, c;
+    public:
+        number(){}
+        number(int p, int q, int r){
+            a = p;
+            b = q;
+            c = r;
+        }
+        void display(){
+            cout<<a<<" "<<b<<" "<<c<<endl;
+        }
+        friend number operator - (number);
+};
+number operator - (number n){
+    number temp;
+    temp.a = -n.a;
+    temp.b = -n.b;
+    temp.c = -n.c;
+    return temp;
+}
+int main(){
+    number n1(10, 5, 6);
+    number n2 = -n1;
+    n1.display();
+    n2.display();
+    return 0;
+}
+
+/* OUTPUT
+10 5 6
+-10 -5 -6
+*/
