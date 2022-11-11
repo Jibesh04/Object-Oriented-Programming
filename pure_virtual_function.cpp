0dfd7efcad475c92bcc5db8e1fe88c30edfaacdc
Jibesh Kumar Panda <99865910+Jibesh04@users.noreply.github.com>
2022-11-11 23:17:33 +0530

    Pure Virtual Function

--- pure_virtual_function.cpp
+++ pure_virtual_function.cpp
@@ -31,5 +31,58 @@ void Book::input(){
     cin>>pages;
 }
 void Book::display(){
-    cout<<"Book Title: "<<title<<"\nPrice: Rs. "<<price<<"\nPages: "<<pages<<endl;
+    cout<<"Book Title: "<<title<<"Price: Rs. "<<price<<"\nPages: "<<pages<<endl;
 }
+class Tape:public Media{
+        float play_time;
+    public:
+        void input();
+        void display();
+};
+void Tape::input(){
+    cout<<"Enter the title: ";
+    if(i>0)
+        fgets(title, 30, stdin);
+    fgets(title, 30, stdin);
+    i++;
+    cout<<"Enter Price: Rs. ";
+    cin>>price;
+    cout<<"Play Time (seconds): ";
+    cin>>play_time;
+}
+void Tape::display(){
+    cout<<"Tape Title: "<<title<<"Price: Rs. "<<price<<"\nPlay Time: "<<play_time<<" s\n";
+}
+int main(){
+    Media *ptr;
+    cout<<"For Book...\n";
+    Book b1;
+    ptr = &b1;
+    ptr -> input();
+    ptr -> display();
+    cout<<"For Tape...\n";
+    Tape t1;
+    ptr = &t1;
+    ptr -> input();
+    ptr -> display();
+    return 0;
+}
+
+/* TERMINAL
+PS D:\Junior\Study\Object-Oriented-Programming> g++ -o pure_virtual_function.exe .\pure_virtual_function.cpp
+PS D:\Junior\Study\Object-Oriented-Programming> .\pure_virtual_function.exe
+For Book...
+Enter the title: The New Beginning
+Enter Price: Rs. 2355
+No. of Pages: 1205
+Book Title: The New Beginning
+Price: Rs. 2355
+Pages: 1205
+For Tape...
+Enter the title: Hello, World! 
+Enter Price: Rs. 230 
+Play Time (seconds): 9999.5
+Tape Title: Hello, World!
+Price: Rs. 230
+Play Time: 9999.5 s
+*/
